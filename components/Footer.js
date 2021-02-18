f4af0d7047636902e8fe8b69954b6f84b407ee57
krishnaclosalair <krishnaclosalair@gmail.com>
2021-02-18 14:48:08 +0000

    Fix footer vertical align

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,34 @@ const currentYear = date.getFullYear();
 const Footer = () => {
   return (
     <>
-      <footer className="bg-black w-full h-16">
-        <div className="max-w-screen-lg mx-auto py-4 px-12">
-          <p className="text-white text-xs">Copyright {currentYear} &bull; <a className="duration-300 hover:text-floss-pink hover:underline" href="https://www.pure360.com/cookies" target="_blank">Cookie Policy</a> &bull; <a className="duration-300 hover:text-floss-pink hover:underline" href="https://www.pure360.com/privacy" target="_blank">Privacy Policy</a> &bull; <a className="duration-300 hover:text-floss-pink hover:underline" href="https://www.pure360.com/licence-agreement" target="_blank">Licence Agreement</a></p>
+      <footer className="bg-black w-full h-16 flex items-center">
+        <div className="max-w-screen-lg px-12">
+          <p className="text-white text-xs">
+            Copyright {currentYear} &bull;{" "}
+            <a
+              className="duration-300 hover:text-floss-pink hover:underline"
+              href="https://www.pure360.com/cookies"
+              target="_blank"
+            >
+              Cookie Policy
+            </a>{" "}
+            &bull;{" "}
+            <a
+              className="duration-300 hover:text-floss-pink hover:underline"
+              href="https://www.pure360.com/privacy"
+              target="_blank"
+            >
+              Privacy Policy
+            </a>{" "}
+            &bull;{" "}
+            <a
+              className="duration-300 hover:text-floss-pink hover:underline"
+              href="https://www.pure360.com/licence-agreement"
+              target="_blank"
+            >
+              Licence Agreement
+            </a>
+          </p>
         </div>
       </footer>
     </>
