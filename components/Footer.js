3443e5459be4fff0248bf21ef25ff1fb9ee0c810
krishnaclosalair <krishnaclosalair@gmail.com>
2021-02-18 13:24:37 +0000

    Add footer links

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <>
       <footer className="bg-black w-full h-12">
         <div className="max-w-screen-lg mx-auto py-4 px-12">
-          <p className="text-white text-xs">Copyright {currentYear}</p>
+          <p className="text-white text-xs">Copyright {currentYear} &bull; <a className="duration-300 hover:text-floss-pink hover:underline" href="https://www.pure360.com/cookies" target="_blank">Cookie Policy</a> &bull; <a className="duration-300 hover:text-floss-pink hover:underline" href="https://www.pure360.com/privacy" target="_blank">Privacy Policy</a> &bull; <a className="duration-300 hover:text-floss-pink hover:underline" href="https://www.pure360.com/licence-agreement" target="_blank">Licence Agreement</a></p>
         </div>
       </footer>
     </>
