a250b77e1bbd7d07ee45c55bed831c85c0cde291
krishnaclosalair <krishnaclosalair@gmail.com>
2021-02-25 12:47:45 +0000

    Update examples

--- pages/examples/custom.js
+++ pages/examples/custom.js
@@ -7,8 +7,19 @@ const custom = () => {
   return (
     <motion.div variants={parentVariants} initial="hidden" animate="visible">
       <section className="max-w-screen-xl mx-auto mb-32 px-4 sm:px-8">
-        <motion.article variants={childrenVariants} className="border-gray-50 border-4 rounded-lg">
-          <iframe className="w-full" style={{height: '75vh'}} src="https://www.uploadlibrary.com/creativepure360/customer-templates/index.html?customName=Pure%20360&profileName=creativepure360&templateID=demo"></iframe>
+        <motion.article
+          variants={childrenVariants}
+          className="border-gray-50 border-4 rounded-lg hidden sm:block"
+        >
+          <iframe
+            className="w-full h-4/5-screen"
+            src="https://www.uploadlibrary.com/creativepure360/customer-templates/index.html?customName=Pure360&profileName=creativepure360&templateID=demo"
+          ></iframe>
+        </motion.article>
+        <motion.article variants={childrenVariants} className="block sm:hidden">
+          <h2 className="font-avant-garde-bold text-2xl sm:text-4xl leading-8 sm:leading-10 text-center mb-3 mx-auto max-w-2xl">
+            Not available on mobile. Please view on Desktop.
+          </h2>
         </motion.article>
       </section>
       <ExitPreview />
--- pages/examples/drag-and-drop.js
+++ pages/examples/drag-and-drop.js
@@ -7,7 +7,12 @@ const dragAndDrop = () => {
   return (
     <motion.div variants={parentVariants} initial="hidden" animate="visible">
       <section className="max-w-screen-lg mx-auto mb-32 px-4 sm:px-8">
-        <motion.article variants={childrenVariants}></motion.article>
+        <motion.article variants={childrenVariants}>
+          <video width="640" controls autoplay="true" className="mx-auto border-gray-100 border-4">
+            <source src="https://www.uploadlibrary.com/creativepure360/Creative/pure360-drag-and-drop-demo.mov" />
+            Your browser does not support HTML video.
+          </video>
+        </motion.article>
       </section>
       <ExitPreview />
     </motion.div>
