e5ad5cf38ea873209cdde72ce72f85bbda6d1c97
krishnaclosalair <krishnaclosalair@gmail.com>
2021-02-25 11:09:39 +0000

    Update examples

--- pages/examples/custom.js
+++ pages/examples/custom.js
@@ -6,9 +6,9 @@ import ExitPreview from "../../components/ExitPreview";
 const custom = () => {
   return (
     <motion.div variants={parentVariants} initial="hidden" animate="visible">
-      <section className="max-w-screen-lg mx-auto mb-32 px-4 sm:px-8">
+      <section className="max-w-screen-xl mx-auto mb-32 px-4 sm:px-8">
         <motion.article variants={childrenVariants} className="border-gray-50 border-4 rounded-lg">
-          <iframe className="w-full h-screen" src="https://www.uploadlibrary.com/creativepure360/customer-templates/index.html?customName=Pure%20360&profileName=creativepure360&templateID=b9c84d06-df5f-4c4c-83a2-0d646119c881"></iframe>
+          <iframe className="w-full" style={{height: '75vh'}} src="https://www.uploadlibrary.com/creativepure360/customer-templates/index.html?customName=Pure%20360&profileName=creativepure360&templateID=demo"></iframe>
         </motion.article>
       </section>
       <ExitPreview />
--- pages/examples/drag-and-drop.js
+++ pages/examples/drag-and-drop.js
@@ -1,5 +1,17 @@
+import { motion } from "framer-motion";
+import { childrenVariants, parentVariants } from "../../variants/variants";
+
+import ExitPreview from "../../components/ExitPreview";
+
 const dragAndDrop = () => {
-  return <></>;
+  return (
+    <motion.div variants={parentVariants} initial="hidden" animate="visible">
+      <section className="max-w-screen-lg mx-auto mb-32 px-4 sm:px-8">
+        <motion.article variants={childrenVariants}></motion.article>
+      </section>
+      <ExitPreview />
+    </motion.div>
+  );
 };
 
 export default dragAndDrop;
