c79e3d72081e331bc1832a729a22eefef8b024ff
Krishna Lair <krishnaclosalair@gmail.com>
2023-01-04 09:32:31 +0000

    Add apiVersion for Sanity

--- client-config.js
+++ client-config.js
@@ -3,6 +3,7 @@ import sanityClient from "@sanity/client";
 const client = sanityClient({
   projectId: "w5xsgj13",
   dataset: "production",
+  apiVersion: "2023-01-01",
   token: "",
   useCdn: true,
 });
