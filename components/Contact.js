d3f7148e79df2497b617550feebbd6dedb64925c
Krishna Lair <krishnaclosalair@gmail.com>
2023-03-09 16:49:39 +0000

    Update contact component

--- components/Contact.js
+++ components/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
         <section className="max-w-[1200px] mx-auto py-[50px] px-[30px] text-center">
           <motion.article variants={childrenVariants}>
             <h1 className="font-greycliff text-[#002a4d] text-[34px] sm:text-[40px] leading-[1.1] font-bold mb-[30px]">Get in touch</h1>
-            <p className="font-opensans text-[18px]leading-6 text-[#002a4d] mb-[30px]">Whether you have a question, suggestion, or just want to say hello, our team is ready to hear from you.</p>
+            <p className="font-opensans text-[18px]leading-6 text-[#002a4d] mb-[30px]">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
             <Link className="font-opensans text-[18px] leading-[28.8px] font-bold duration-300 ease-in-out text-white hover:text-[#002a4d] bg-[#002a4d] hover:bg-white rounded-3xl inline-block select-none px-[25px] py-[9px]" href="mailto:accountmanagers@spotler.co.uk " scroll={false}>
               Contact us
             </Link>
