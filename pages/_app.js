983d95a12f9fa8b8492f6c5ea13c97ab3f0311e5
Krishna Lair <krishnaclosalair@gmail.com>
2023-08-23 15:04:25 +0100

    Update head

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Script from "next/script";
+import Head from "next/head";
 import "../styles/styles.css";
 
 import Header from "../components/Header";
@@ -8,6 +9,14 @@ import Footer from "../components/Footer";
 const App = ({ Component, pageProps, router }) => {
   return (
     <>
+      <Head>
+        <title>Spotler Creative Playground</title>
+        <meta name="description" content="Find a broad array of projects we've completed, blog containing how-to guides and useful tips in using our email editors, and tools we've developed to help you with your email marketing." />
+        <meta property="og:title" content="Spotler Creative Playground" />
+        <meta property="og:description" content="Find a broad array of projects we've completed, blog containing how-to guides and useful tips in using our email editors, and tools we've developed to help you with your email marketing." />
+        <meta property="og:url" content="https://creative.spotler.co.uk/" />
+        <meta property="og:type" content="website" />
+      </Head>
       <Header />
       <Main router={router}>
         <Component {...pageProps} />
