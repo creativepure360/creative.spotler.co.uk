1041aa3df3c3a4c7f22701933e83026cd3bf787f
Krishna Lair <krishnaclosalair@gmail.com>
2024-09-26 13:10:45 +0100

    Update api version

--- client-config.js
+++ client-config.js
@@ -3,7 +3,7 @@ import {createClient} from "@sanity/client";
 const client = createClient({
   projectId: "tgvb7jy1",
   dataset: "production",
-  apiVersion: "2024-01-01",
+  apiVersion: "2025-01-01",
   token: "",
   useCdn: true,
 });
